7891e194fcae4ee16e658f801cdf946b3d69d49b
3bdalrahman <abdomoslaam@gmail.com>
2023-11-08 20:17:55 +0200

    start

---
+++ MillionWinner/src/main/java/com/mycompany/millionwinner/MillionWinner.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.millionwinner;
+
+/**
+ *
+ * @author user
+ */
+public class MillionWinner {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
